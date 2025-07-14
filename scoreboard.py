c52fe1de4aa737cbaacd5ceeb37b7a7b12547812
Shahbazsk1 <shahbazsk2003@gmail.com>
2025-07-14 15:33:40 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,3 @@
-# create the scoreboard
-
-# Create a scoreboard that keeps track of which level the user is on.
-# Every time the turtle player does a successful crossing, the level
-# should increase. When the turtle hits a car, GAME OVER should be
-# displayed in the centre. If you get stuck, check the video walkthrough
-# in Step 7.
-
 from turtle import Turtle
 
 FONT = ("Courier", 24, "normal")
